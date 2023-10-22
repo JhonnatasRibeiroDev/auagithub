2a8540b324111e5a335b762a2998a83c8265595b
Jhonnatas Ribeiro <jhonnatasribeiro01@gmail.com>
2023-10-22 02:19:04 -0300

    Aplication simple in java

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //vetor
+        String[]vetor = new String[]{"a","b","c","d","e","f","g","h","i","j"};
+
+        for(String letra: vetor){
+            System.out.println(letra);
+        }
+        
+    }
+}
